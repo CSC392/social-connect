8e170c717d2cd45c54a7bd34c3c81458d57536b4
Alex Lamouri <salemalex@hotmail.com>
2020-10-08 21:36:04 -0400

    add create-react-app code to dev branch

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Social Connect
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/emilierobichaud1/social-connect"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit our repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
