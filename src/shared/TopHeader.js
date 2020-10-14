8d9726a4a7f7eb41cfde52acd10c280ac5b8e3df
NavyaGupta <navya20gupta@gmail.com>
2020-10-13 22:30:31 -0400

    Add top header

---
+++ src/shared/TopHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  header: {
+    fontSize: "30px",
+    marginTop: "10px",
+    borderBottom: `1px solid`,
+  },
+});
+
+export const TopHeader = () => {
+  const classes = useStyles({});
+  return <p className={classes.header}> Social Connect</p>;
+};
