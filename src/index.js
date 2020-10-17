82c1e7a19251023ab68399497e993bda13c75b54
Alex Lamouri <salemalex@hotmail.com>
2020-10-17 18:35:58 -0400

    abstract chessboard into component

--- src/index.js
+++ src/index.js
@@ -1,42 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { BackButton } from "./shared/BackButton";
-import { TopHeader } from "./shared/TopHeader";
-import Chessboard from "chessboardjsx";
-
-const Header = (props) => <h1> {props.text} </h1>
-const Button = (props) => <button onClick={props.handleClick}> {props.text} </button>
-
-const Board = (props) => {
-  if (props.boardState === 'reset') {
-    return(<div> < Chessboard /> </div>)
-  }
-  else if (props.boardState === 'set') {
-    return(<div> < Chessboard position="start" /> </div>)
-  }
-  else {
-    return(<div> Error </div>)
-  }
-}
+import { BackButton } from "./components/BackButton";
+import { TopHeader } from "./components/TopHeader";
+import { Chessboard } from "./components/Chessboard";
 
 const App = () => {
-  const [boardState, setBoardState] = useState('reset')
-
-  const resetBoard = () => {
-    setBoardState('reset')
-  }
-
-  const setBoard = () => {
-    setBoardState('set')
-  }
-
-
+  
   return(
     <div>
-      < Header text="Social Connect - Play" />
-      < Button handleClick={resetBoard} text="reset board" />
-      < Button handleClick={setBoard} text="set board" />
-      < Board boardState={boardState} />
+      < TopHeader text="Social Connect - Play" />
+      < Chessboard />
     </div> 
   )
 }
