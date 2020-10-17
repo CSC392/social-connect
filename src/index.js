9cb810e98a0268d9ca1c086bd8607416df241b02
Alex Lamouri <salemalex@hotmail.com>
2020-10-17 19:10:12 -0400

    add basic demo for conditional rendering & views

--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BackButton } from "./shared/BackButton";
 import { TopHeader } from "./shared/TopHeader";
 
-const App = () => (
-  <div>
-    {/* This is just to test. Remove later */}
-    <TopHeader></TopHeader>
-    {/* This is just to test if prop is passing. We will change this our page instead of alert */}
-    <BackButton
-      onClick={() => {
-        alert("Yay, it works");
-      }}
-    />
-  </div>
-);
+const Title = (props) => <h1> {props.text} </h1>
+const Button = (props) => <button onClick={props.handleClick}> {props.text} </button>
+
+const App = () => {
+
+  const [view, setView] = useState('menu')
+  const views = ['menu', 'host', 'play']
+  
+  const goBack = () => {
+    var prevView = views[views.findIndex(k => k === view) - 1]
+    setView(prevView)
+  }
+
+  const goHost = () => {
+    setView('host')
+  }
+
+  const goPlay = () => {
+    setView('play')
+  }
+
+  switch(view) {
+
+    case 'menu':
+      return(
+        <div>
+          <Title text="Menu" />
+          <Button handleClick={goHost} text="Host Game" />
+          <Button text="Browse Games" />
+          <Button text="Join Game" />
+        </div>
+      ) 
+
+    case 'host':
+      return(
+        <div>
+          <Title text="Host" />
+          <Button handleClick={goBack} text="Back" />
+          <Button handleClick={goPlay} text="Play" />
+        </div>
+      ) 
+
+    case 'play':
+      return(
+        <div>
+          <Title text="Play" />
+          <Button handleClick={goBack} text="Back" />
+        </div>
+      ) 
+
+    default:
+      return <div> Error </div>
+
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
