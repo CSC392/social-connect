a4660ca102664c0c0ed470f1d2b0f53221665d5c
Alana Hodge <alanahodge0@gmail.com>
2020-10-23 22:10:39 -0400

    Refactored Home Page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HomePage } from "./HomePage"
-//import { BackButton } from "./shared/BackButton";
-//import { TopHeader } from "./shared/TopHeader";
-
-//const App = () => (
-  //<div>
-    {/* This is just to test. Remove later */}
-   //<TopHeader></TopHeader>
-    //{/* This is just to test if prop is passing. We will change this our page instead of alert */}
-    //<BackButton
-      //onClick={() => {
-        //alert("Yay, it works");
-      //}}
-    ///>
-  //</div>
-//);
 
 const App = () => (
   <div>
