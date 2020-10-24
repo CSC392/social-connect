512adc602f3e44feb34c0b76a0b4bd9a2a7f1543
Alana Hodge <alanahodge0@gmail.com>
2020-10-23 22:10:18 -0400

    Refactored Home Page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
-import { Button, makeStyles, useEventCallback} from "@material-ui/core";
-import { TopHeader } from "./shared/TopHeader";
-import chessBoard from "./hostGamePage/chessBoard.png";
+import React from "react";
+import { Button, makeStyles } from "@material-ui/core";
+import { TopHeader } from "./components/TopHeader";
+import chessBoard from "./components/hostGamePage/chessBoard.png";
 import Box from '@material-ui/core/Box';
-import { flexbox } from '@material-ui/system';
-
 
 const styles = makeStyles({
-  backButton: {
-    marginTop: "10px",
-  },
   body: {
     paddingTop: "50px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
-  PlayButton: {
+  playButton: {
     backgroundColor: "#C8BFE7",
     fontSize: "25px",
     marginTop: "10px",
@@ -26,35 +21,36 @@ const styles = makeStyles({
       backgroundColor: "#8474BE",
     },
   },
-  WelcomeBanner: {
+  welcomeBanner: {
     fontSize: "40px",
     paddingTop: "5px",
     display: "flex",
     alignItems: "center",
     flexDirection: "column",
   },
+  box: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  miniBox: {
+    paddingRight: "100px",
+  }
   
 })
-
-//class LoggingButton extends React.Component{
-//handleClick = () => {
-  //  console.log('The Button was clicked');
-  //}
-//}
-
 
 export const HomePage = () => {
   const classes = styles({});
   return (
     <div>
       <TopHeader />
-      <div className={classes.WelcomeBanner}>
+      <div className={classes.welcomeBanner}>
         <p>Welcome To Social Connect</p>
         <img src={chessBoard} />
       </div>
       
       <div className={classes.body}>
-        <Button className={classes.PlayButton}>
+        <Button className={classes.playButton}>
           Start Playing
         </Button>
         
@@ -62,17 +58,17 @@ export const HomePage = () => {
       </div>
 
       <div style={{ width: '100%' }}>
-      <Box display="flex" p={1} flexDirection="row" justifyContent="center" >
-        <Box p={1} paddingRight = "100px">
+      <Box className={classes.box} p={1}> 
+        <Box p={1} className={classes.miniBox}>
           <h2>Connect With Friends or Strangers</h2>
         </Box>
-        <Box p={1} paddingRight="100px">
+        <Box p={1} className={classes.miniBox}>
           <h2>Communicate via Chat</h2>
         </Box>
-        <Box p={1} paddingRight="100px">
+        <Box p={1} className={classes.miniBox}>
           <h2>Play a Variety of Games</h2>
         </Box>
-        <Box p={1}>
+        <Box p={1} className={classes.miniBox}>
           <h2>More Features Coming Soon</h2>
         </Box>
       </Box>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BackButton } from "./components/BackButton";
-import { TopHeader } from "./components/TopHeader";
-import { Chessboard } from "./components/Chessboard";
+import { HomePage } from "./HomePage"
+//import { BackButton } from "./shared/BackButton";
+//import { TopHeader } from "./shared/TopHeader";
 
-const App = () => {
-  return (
-    <div>
-      {/* Add Home Page here */}
-      <TopHeader text="Social Connect - Play" />
-      <Chessboard />
-    </div>
-  );
-};
+//const App = () => (
+  //<div>
+    {/* This is just to test. Remove later */}
+   //<TopHeader></TopHeader>
+    //{/* This is just to test if prop is passing. We will change this our page instead of alert */}
+    //<BackButton
+      //onClick={() => {
+        //alert("Yay, it works");
+      //}}
+    ///>
+  //</div>
+//);
+
+const App = () => (
+  <div>
+    <HomePage></HomePage>
+    
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
