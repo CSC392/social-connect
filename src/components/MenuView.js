bb33bd53562765a3e46a73f29c3e8491748694e7
Alex Lamouri <salemalex@hotmail.com>
2020-11-03 21:09:07 -0500

    Revert "Revert "Merge pull request #25 from emilierobichaud1/SC-37""
    This reverts commit 867574362a1ec4136ad92e7fcf6f8ae3b09a795f.

--- src/components/MenuView.js
+++ src/components/MenuView.js
@@ -1,26 +1,29 @@
-import React from "react";
-import { Button, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, TextField, Dialog, DialogTitle } from "@material-ui/core";
 import { TopHeader } from "./TopHeader";
 import { Link } from "react-router-dom";
 import { MenuViewStyles } from "../styles/MenuViewStyles";
 
 export const MenuView = (props) => {
   const classes = MenuViewStyles({});
-
+  const [showJoinMenu, setShowJoinMenu] = useState(false);
+  const [joinCode, setJoinCode] = useState("");
   const handleChange = (event) => {
     props.setUsername(event.target.value);
   };
+  const handleJoinChange = (event) => {
+    setJoinCode(event.target.value);
+  };
   const validUsername = props.username ? false : true;
+  const validJoinCode = joinCode ? false : true;
   return (
     <div>
       <TopHeader />
-
       <div className={classes.backButton}>
         <Link to="/" className={classes.link}>
           <Button className={classes.Backbutton}>Back</Button>
         </Link>
       </div>
-
       <div className={classes.body}>
         <TextField
           variant="outlined"
@@ -50,11 +53,26 @@ export const MenuView = (props) => {
           className={classes.joinButton}
           disabled={validUsername}
           onClick={() => {
-            props.goNext("menu", "join");
+            setShowJoinMenu(true);
           }}
         >
           Join Game
         </Button>
+        {showJoinMenu && (
+          <Dialog open={true}>
+            <DialogTitle>
+              <TextField
+                variant="outlined"
+                label="Enter Game Code"
+                onChange={handleJoinChange}
+                error={validJoinCode}
+              ></TextField>
+            </DialogTitle>
+            <Button disabled={validJoinCode} className={classes.joinCodeButton}>
+              <Link to="/play/game">Join Game</Link>
+            </Button>
+          </Dialog>
+        )}
       </div>
     </div>
   );
